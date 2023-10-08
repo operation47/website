3d1692902aaed2a5a2f230ca5ce85aa5bf079dda
Mikka <mikka@sendke.eu>
2023-10-08 04:49:49 +0200

    timestamps were pain but it should work now maybe idk

--- index.js
+++ index.js
@@ -77,9 +77,7 @@ function buildChatMessageHTML(time, username, message) {
 }
 
 function unixTimeTo2Hour2Minute(time, timeZone) { // was ein drecks name
-    console.log(time);
-    console.log("type: " + typeof time);
-    console.log(new Date(time).toLocaleTimeString(timeZone, { hour: '2-digit', minute: '2-digit' }));
+    return new Date(time).toLocaleTimeString(timeZone, { hour: '2-digit', minute: '2-digit' });
 }
 
 app.get('/', async (req, res) => {
