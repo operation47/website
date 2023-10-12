3cb88c56fdf6ed9a232845088355d35b2d78ea35
Darius <darius>
2023-10-13 00:10:40 +0200

    Generic Commit; Most likely a fix or small feature

--- base.js
+++ base.js
@@ -2,10 +2,10 @@ export function createHTMLElement(type, options, classList) {
     const element = document.createElement(type);
     if (options) {
         for (const key in options) {
-            if (key === innerHTML) {
-                element.innerHTML = options[key];
-            } else if (key === textContent) {
+            if (key === textContent) {
                 element.textContent = options[key];
+            } else if (key === innerHTML) {
+                element.innerHTML = options[key];
             } else {
                 element.setAttribute(key, options[key]);
             }
