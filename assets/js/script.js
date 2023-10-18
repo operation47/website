bb300f00ea725724c6af76bbb216753c6856999b
GenJoeyy <bgirgis1000@gmail.com>
2023-10-19 01:50:35 +0200

    small fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected to the server');
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from the server');
+});
+
 document.body.addEventListener('click',(e) => { 
     const menuCheckbox = document.getElementById('menu-checkbox');
     if (e.target !== menuCheckbox) {
