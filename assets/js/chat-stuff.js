7248ea37c5fe22d3be69a5fec7307a3069419f03
GenJoeyy <bgirgis1000@gmail.com>
2023-10-19 06:37:38 +0200

    small fix

---
+++ assets/js/chat-stuff.js
@@ -0,0 +1,22 @@
+import { loadAllChatMessages } from "./message-handler.js"
+import { socket } from "./script.js"
+
+async function main() {
+    await loadAllChatMessages();
+}
+main();
+
+socket.on('viewerCount', (count) => {
+    document.getElementById('viewer-count').textContent = getCountText(count)
+});
+
+socket.on('newMessage', () => {
+    loadAllChatMessages();
+});
+
+function getCountText(count) {
+    return count == 1 ? count + ' hat grad FOMO' : count + ' haben grad FOMO'
+}
+
+
+
