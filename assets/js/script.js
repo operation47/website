ce5e6e5f892bc893afdd8a7e177bbca115222a63
Mikka <mikka@sendke.eu>
2023-10-28 23:24:22 +0200

    mhmhmmhm

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,9 @@ socket.on("connect", () => {
     const info = document.getElementsByClassName("disconnected-info")[0];
     if(!info.classList.contains("hidden")) {
         info.classList.add("hidden");
+        if(dotInterval) {
+            clearInterval(dotInterval);
+        }
     }
 });
 
@@ -14,12 +17,25 @@ socket.on("disconnect", () => {
     const info = document.getElementsByClassName("disconnected-info")[0];
     if(info.classList.contains("hidden")) {
         info.classList.remove("hidden");
+        dotInterval = setInterval(cycleDots, 500);
     }
 });
 
 window.addEventListener('pageshow', () => {
     if (!socket.connected) {
         socket.connect();
+    }
+});
+
+setInterval(() => {
+    if(!socket.connected) {
+        socket.connect();
+    }
+}, 5000);
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        socket.disconnect();
     }
 });
 
@@ -33,3 +49,17 @@ document.body.addEventListener(
     },
     false,
 );
+
+
+const dots = document.getElementsByClassName("dots")[0];
+let dotInterval;
+let dotIndex = 0;
+function cycleDots() {
+    dotIndex = (dotIndex + 1) % 4;
+    dots.innerHTML = ".".repeat(dotIndex);
+}
+
+const disconnect_info = document.getElementsByClassName("disconnected-info")[0];
+setTimeout(() => {
+    disconnect_info.classList.remove("prevent-animation");
+}, 250);
