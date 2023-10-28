d3beab777c7112fcb004af5aae204c04a933321b
Mikka <mikka@sendke.eu>
2023-10-28 19:59:07 +0200

    added disconnected info

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,24 @@ export { socket };
 
 socket.on("connect", () => {
     console.log("Connected to the server");
+    const info = document.getElementsByClassName("disconnected-info")[0];
+    if(info.classList.contains("hidden")) {
+        info.classList.add("hidden");
+    }
 });
+
 socket.on("disconnect", () => {
     console.log("Disconnected from the server");
+    const info = document.getElementsByClassName("disconnected-info")[0];
+    if(info.classList.contains("hidden")) {
+        info.classList.remove("hidden");
+    }
+});
+
+document.addEventListener("keyup", (e) => {
+    if(e.code === "KeyT") {
+        socket.disconnect();
+    }
 });
 
 document.body.addEventListener(
