d8b148e82ef6b15600287a7a5ec69654525963ad
Mikka <mikka@sendke.eu>
2023-10-29 11:27:06 +0100

    small change

--- assets/js/chat-stuff.js
+++ assets/js/chat-stuff.js
@@ -17,3 +17,51 @@ socket.on("newMessage", (channel) => {
 function getCountText(count) {
     return count == 1 ? count + " hat grad FOMO" : count + " haben grad FOMO";
 }
+
+const disconnect_info = document.getElementsByClassName("disconnected-info")[0];
+const dots = document.getElementsByClassName("dots")[0];
+let dotInterval;
+let dotIndex = 0;
+
+function cycleDots() {
+    dotIndex = (dotIndex + 1) % 4;
+    dots.innerHTML = ".".repeat(dotIndex);
+}
+
+socket.on("connect", () => {
+    if(!disconnect_info.classList.contains("hidden")) {
+        if(dotInterval) {
+            clearInterval(dotInterval);
+        }
+        disconnect_info.classList.add("green");
+        setTimeout(() => {
+            disconnect_info.classList.add("hidden");
+        }, 250);
+    }
+});
+
+socket.on("disconnect", () => {
+    if(disconnect_info.classList.contains("hidden")) {
+        disconnect_info.classList.remove("hidden");
+        disconnect_info.classList.remove("green");
+        dotInterval = setInterval(cycleDots, 500);
+    }
+});
+
+window.addEventListener('pageshow', () => {
+    if (!socket.connected) {
+        socket.connect();
+    }
+});
+
+setTimeout(() => {
+    if(!disconnect_info) return;
+    disconnect_info.classList.remove("prevent-animation");
+}, 250);
+
+setInterval(() => {
+    if(!socket.connected) {
+        socket.connect();
+    }
+}, 5000);
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,39 +3,11 @@ export { socket };
 
 socket.on("connect", () => {
     console.log("Connected to the server");
-    const info = document.getElementsByClassName("disconnected-info")[0];
-    if(!info.classList.contains("hidden")) {
-        if(dotInterval) {
-            clearInterval(dotInterval);
-        }
-        info.classList.add("green");
-        setTimeout(() => {
-            info.classList.add("hidden");
-        }, 250);
-    }
 });
 
 socket.on("disconnect", () => {
     console.log("Disconnected from the server");
-    const info = document.getElementsByClassName("disconnected-info")[0];
-    if(info.classList.contains("hidden")) {
-        info.classList.remove("hidden");
-        info.classList.remove("green");
-        dotInterval = setInterval(cycleDots, 500);
-    }
 });
-
-window.addEventListener('pageshow', () => {
-    if (!socket.connected) {
-        socket.connect();
-    }
-});
-
-setInterval(() => {
-    if(!socket.connected) {
-        socket.connect();
-    }
-}, 5000);
 
 document.body.addEventListener(
     "click",
@@ -47,18 +19,4 @@ document.body.addEventListener(
     },
     false,
 );
-
-
-const dots = document.getElementsByClassName("dots")[0];
-let dotInterval;
-let dotIndex = 0;
-function cycleDots() {
-    dotIndex = (dotIndex + 1) % 4;
-    dots.innerHTML = ".".repeat(dotIndex);
-}
-
-const disconnect_info = document.getElementsByClassName("disconnected-info")[0];
-setTimeout(() => {
-    disconnect_info.classList.remove("prevent-animation");
-}, 250);
 
